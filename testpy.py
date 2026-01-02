f367dfdb9c21bddf7e16ca108720ced113f2170d
gytdrop <shaikaktharali2022@gmail.com>
2026-01-02 19:38:06 +0530

    again added as an exaplmle

--- testpy.py
+++ testpy.py
@@ -2,3 +2,10 @@ print("Testing in cmd")
 print("test") 
 print("test") 
 print("test") 
+''' ! [rejected]        main -> main (fetch first)
+error: failed to push some refs to 'https://github.com/gytdrop/test.git'
+hint: Updates were rejected because the remote contains work that you do not
+hint: have locally. This is usually caused by another repository pushing to
+hint: the same ref. If you want to integrate the remote changes, use
+hint: 'git pull' before pushing again.
+hint: See the 'Note about fast-forwards' in 'git push --help' for details.'''
